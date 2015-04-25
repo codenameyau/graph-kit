3da31816c0affbfd34e5fd91bed6c51b7da2ed84
Jorge Yau <jyau01@citymail.cuny.edu>
2015-04-25 01:10:59 -0400

    Resolves #2 - Added build packaging with gulp

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+/*!
+ * graph-kit - gulpfile.js
+ * MIT License (c) 2015
+ */
+'use strict';
+
+// Load dependecies
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var rimraf = require('rimraf');
+var strReplace = require('gulp-replace');
+
+// [Task] removes 'build/'
+gulp.task('clean', function(cb) {
+  rimraf('build/', cb);
+});
+
+// [Task] uglify js
+gulp.task('uglify', function() {
+  return gulp.src('public/graph-kit.js')
+    .pipe(strReplace(/'use strict';/g, ''))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('build'));
+});
+
+// [Task] generates build
+gulp.task('default', ['uglify']);
